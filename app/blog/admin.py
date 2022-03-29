fd0e21fbb009283607160b1453cd8af8e3de7fb5
Gabriel <gabrielx165@gmail.com>
2022-03-29 15:24:50 -0300

    setup admin area for post, tags and categories

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'realesed_date', 'title')
+    list_display_links = ('id', 'author')
+
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
+admin.site.register(models.Post, PostAdmin)
