26ebf954e57704a14135d178360d2c9b52c8fc45
Gabriel <gabrielx165@gmail.com>
2022-03-30 10:23:55 -0300

    initial setup api pagination

--- app/app/settings.py
+++ app/app/settings.py
@@ -95,7 +95,9 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 1
 }
 
 # Password validation
