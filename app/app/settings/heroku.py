94d69d2e849a0131cfb461f15a61f8a549003b26
Gabriel <gabrielx165@gmail.com>
2022-04-15 19:36:11 -0300

    change reference of static files storage on heroku.py

--- app/app/settings/heroku.py
+++ app/app/settings/heroku.py
@@ -29,11 +29,11 @@ if USE_S3:
     # s3 static settings
     STATIC_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    STATICFILES_STORAGE = 'app.storage_backends.StaticStorage'
+    STATICFILES_STORAGE = 'storage_backends.StaticStorage'
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'app.storage_backends.PublicMediaStorage'
+    DEFAULT_FILE_STORAGE = 'storage_backends.PublicMediaStorage'
 else:
     STATIC_URL = '/staticfiles/'
     STATIC_ROOT = BASE_DIR/'staticfiles'
