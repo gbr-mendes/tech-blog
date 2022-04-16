70f17db5b01ca923d850b0a2e77d1a5ccddc2709
Gabriel <gabrielx165@gmail.com>
2022-04-16 09:52:26 -0300

    change js script to comunicate with google oauth endpoint

--- app/templates/static/js/authentication.js
+++ app/templates/static/js/authentication.js
@@ -148,7 +148,7 @@ function oauthSignIn() {
     'client_id': document.getElementsByName('google-signin-client_id')[0].content,
     'redirect_uri': `${window.location.origin}/login`,
     'response_type': 'token',
-    'scope': 'https://www.googleapis.com/openid',
+    'scope': 'https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile',
     'include_granted_scopes': 'true',
     'state': 'pass-through value'
   };
