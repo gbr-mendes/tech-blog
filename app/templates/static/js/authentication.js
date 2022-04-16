49ce52beeccba1fa2fb5f5de5a1451ecf81e477c
Gabriel <gabrielx165@gmail.com>
2022-04-16 08:46:53 -0300

    change js script to comunicate with google oauth endpoint

--- app/templates/static/js/authentication.js
+++ app/templates/static/js/authentication.js
@@ -134,7 +134,38 @@ btnLogout.addEventListener('click',e=> {
 /*
 * Create form to request access token from Google's OAuth 2.0 server.
 */
+function oauthSignIn() {
+  // Google's OAuth 2.0 endpoint for requesting an access token
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
 
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {
+    'client_id': document.getElementsByName('google-signin-client_id')[0].content,
+    'redirect_uri': `${window.location.origin}/login`,
+    'response_type': 'token',
+    // 'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+    'include_granted_scopes': 'true',
+    'state': 'pass-through value'
+  };
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit()
+}
 (function checkSocialAuthentication(){
   const queryString = window.location.hash
   const urlParams = new URLSearchParams(queryString.split('#')[1])
